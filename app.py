3f2cd46828cf790c04985cdad6d7e8b6848129ab
DhruviMandaviya <mdhruvim@gmail.com>
2025-05-05 18:00:07 -0400

    Replaced pdf2image with pymupdf for Streamlit compatibility

--- app.py
+++ app.py
@@ -32,10 +32,11 @@ def text_to_speech(text):
 # Load environment variables from .env file
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_gemini_response(input,pdf_cotent,prompt):
-    model=genai.GenerativeModel('gemini-1.5-flash')
-    response=model.generate_content([input,pdf_content[0],prompt])
+def get_gemini_response(input, pdf_text, prompt):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content([input, pdf_text, prompt])
     return response.text
+
 
 # def input_pdf_setup(uploaded_file):
 #     if uploaded_file is not None:
